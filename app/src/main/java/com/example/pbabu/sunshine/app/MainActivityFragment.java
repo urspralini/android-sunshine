0dd461d227748ce3fd08cf91aeedcacc0a7a0e8e
pbabu <prabhu.babu@elementum.com>
2015-05-09 15:39:15 -0700

    1st chapter project: sample sunshine with a list view and adpater with mock data

--- app/src/main/java/com/example/pbabu/sunshine/app/MainActivityFragment.java
+++ app/src/main/java/com/example/pbabu/sunshine/app/MainActivityFragment.java
@@ -5,6 +5,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
@@ -18,6 +24,28 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        String[] sampleWeatherForecastArr = new String[]{
+                "Today - Sunny 88/63",
+                "Tomorrow - Rainy - 50/40",
+                "Weds - Foggy - 60/50",
+                "Thu - Sunny - 88/63",
+                "Fri - Foggy - 50/40",
+                "Sat - Sunny - 60/50"
+        };
+        View fragmentView = inflater.inflate(R.layout.fragment_main, container, false);
+        List<String> sampleWeatherForecast = Arrays.asList(sampleWeatherForecastArr);
+        ArrayAdapter<String> forecastAdapter = new ArrayAdapter<String>(
+                //current context,fragment's parent activity
+                getActivity(),
+                //id of list item layout
+                R.layout.list_item_forecast,
+                //id of text view to populate
+                R.id.list_item_forecast_textview,
+                //data list
+                sampleWeatherForecast
+        );
+        ListView forecastListView = (ListView)fragmentView.findViewById(R.id.listview_forecast);
+        forecastListView.setAdapter(forecastAdapter);
+        return fragmentView;
     }
 }
