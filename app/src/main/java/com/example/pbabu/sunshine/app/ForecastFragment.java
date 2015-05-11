f21bb3f95ae566907ea2b0fda9188d8d49173f71
pbabu <prabhu.babu@elementum.com>
2015-05-11 02:39:10 -0700

    add refresh menu option

--- app/src/main/java/com/example/pbabu/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/pbabu/sunshine/app/ForecastFragment.java
@@ -1,10 +1,14 @@
 package com.example.pbabu.sunshine.app;
 
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -54,20 +58,53 @@ public class ForecastFragment extends Fragment {
         );
         ListView forecastListView = (ListView)fragmentView.findViewById(R.id.listview_forecast);
         forecastListView.setAdapter(forecastAdapter);
-        FetchWeatherTask fetchWeatherTask = new FetchWeatherTask();
-        fetchWeatherTask.execute();
         return fragmentView;
     }
 
-    private class FetchWeatherTask extends AsyncTask<Void, Void, Void> {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //add this line to enable this fragment to handle menu events
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.forecastfragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.action_refresh) {
+            final String MV_POSTAL_CODE = "94043";
+            FetchWeatherTask fetchWeatherTask = new FetchWeatherTask();
+            fetchWeatherTask.execute(MV_POSTAL_CODE);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void setHasOptionsMenu(boolean hasMenu) {
+        super.setHasOptionsMenu(hasMenu);
+    }
+
+    private class FetchWeatherTask extends AsyncTask<String, Void, Void> {
         private  final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
         @Override
-        protected Void doInBackground(Void... params) {
+        protected Void doInBackground(String... params) {
             // These two need to be declared outside the try/catch
             // so that they can be closed in the finally block.
             HttpURLConnection urlConnection = null;
             BufferedReader reader = null;
 
+            final String DEFAULT_POSTAL_CODE = "95134"; // default postal code
+            String postalCode = null;
+            if(params == null || params.length == 0){
+                postalCode = DEFAULT_POSTAL_CODE;
+            }else {
+                postalCode = params[0];
+            }
             // Will contain the raw JSON response as a string.
             String forecastJsonStr = null;
 
@@ -75,8 +112,23 @@ public class ForecastFragment extends Fragment {
                 // Construct the URL for the OpenWeatherMap query
                 // Possible parameters are available at OWM's forecast API page, at
                 // http://openweathermap.org/API#forecast
-                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7");
-
+                final String BASE_URL = "http://api.openweathermap.org/data/2.5/forecast/daily";
+                final String QUERY_PARAM = "q";
+                final String MODE_PARAM = "mode";
+                final String UNITS_PARAM = "units";
+                final String CNT_PARAM = "cnt";
+                final String modeJson = "json";
+                String unitMetric = "metric";
+                int numOfDays = 7;
+                Uri.Builder uriBuilder = Uri.parse(BASE_URL).buildUpon()
+                        .appendQueryParameter(QUERY_PARAM, postalCode)
+                        .appendQueryParameter(MODE_PARAM, modeJson)
+                        .appendQueryParameter(UNITS_PARAM, unitMetric)
+                        .appendQueryParameter(CNT_PARAM, Integer.toString(numOfDays));
+                Uri uri = uriBuilder.build();
+                String uriStr = uri.toString();
+                Log.v(LOG_TAG, "url:" + uriStr);
+                URL url = new URL(uriStr);
                 // Create the request to OpenWeatherMap, and open the connection
                 urlConnection = (HttpURLConnection) url.openConnection();
                 urlConnection.setRequestMethod("GET");
@@ -104,6 +156,7 @@ public class ForecastFragment extends Fragment {
                     forecastJsonStr = null;
                 }
                 forecastJsonStr = buffer.toString();
+                Log.v(LOG_TAG, forecastJsonStr);
             } catch (IOException e) {
                 Log.e(LOG_TAG, "Error ", e);
                 // If the code didn't successfully get the weather data, there's no point in attempting
