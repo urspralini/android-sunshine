9cf1f53d5bef025641de610782f8de95e9ccb67d
pbabu <prabhu.babu@elementum.com>
2015-05-11 16:11:32 -0700

    Add onItemClickListener and display text in Android toast

--- app/src/main/java/com/example/pbabu/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/pbabu/sunshine/app/ForecastFragment.java
@@ -12,8 +12,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -65,6 +67,14 @@ public class ForecastFragment extends Fragment {
                 sampleWeatherForecast
         );
         ListView forecastListView = (ListView)fragmentView.findViewById(R.id.listview_forecast);
+        //set on item click listener
+        forecastListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String toastText = forecastAdapter.getItem(position);
+                Toast.makeText(view.getContext(), toastText, Toast.LENGTH_SHORT).show();
+            }
+        });
         forecastListView.setAdapter(forecastAdapter);
         return fragmentView;
     }
