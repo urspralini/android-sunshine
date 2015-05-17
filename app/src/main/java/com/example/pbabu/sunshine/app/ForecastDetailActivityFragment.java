154374d6cfe45b7dcfa58256f0201fd4089a8997
pbabu <prabhu.babu@elementum.com>
2015-05-16 22:22:21 -0700

    chapter-3: Add share menu in action bar and actionShareProvider

--- app/src/main/java/com/example/pbabu/sunshine/app/ForecastDetailActivityFragment.java
+++ app/src/main/java/com/example/pbabu/sunshine/app/ForecastDetailActivityFragment.java
@@ -3,7 +3,13 @@ package com.example.pbabu.sunshine.app;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -13,8 +19,12 @@ import android.widget.TextView;
  * A placeholder fragment containing a simple view.
  */
 public class ForecastDetailActivityFragment extends Fragment {
-
+    private static final String LOG_TAG = ForecastDetailActivityFragment.class.getSimpleName();
+    private static final String HASH_TAG = "#sunshineApp";
+    private String mForecastStr;
+    private ShareActionProvider mShareActionProvider;
     public ForecastDetailActivityFragment() {
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -29,9 +39,29 @@ public class ForecastDetailActivityFragment extends Fragment {
         TextView textView = (TextView) forecastDetailFragment.findViewById(R.id.detail_text);
         Intent intent = getActivity().getIntent();
         if(intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
-            String forecastText = intent.getStringExtra(Intent.EXTRA_TEXT);
-            textView.setText(forecastText);
+            mForecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+            textView.setText(mForecastStr);
         }
         return forecastDetailFragment;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.forecast_detail_fragment, menu);
+        MenuItem shareMenuItem = menu.findItem(R.id.action_share);
+        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(shareMenuItem);
+        if(mShareActionProvider != null) {
+            mShareActionProvider.setShareIntent(buildShareIntent());
+        }else {
+            Log.d(LOG_TAG, "share action provider is null?");
+        }
+    }
+
+    private Intent buildShareIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND)
+                .addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET)
+                .setType("text/plain")
+                .putExtra(Intent.EXTRA_TEXT, mForecastStr+HASH_TAG);
+        return shareIntent;
     }
 }
